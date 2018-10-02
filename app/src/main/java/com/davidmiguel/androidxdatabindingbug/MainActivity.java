e1d09f6f74244af7720ea7f1a864b6bebb39bdbb
David Miguel Lozano <me@davidmiguel.com>
2018-10-02 17:03:57 +0200

    Sample app target API27 and using support lib

---
+++ app/src/main/java/com/davidmiguel/androidxdatabindingbug/MainActivity.java
@@ -0,0 +1,47 @@
+package com.davidmiguel.androidxdatabindingbug;
+
+import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
+import android.support.text.emoji.EmojiCompat;
+import android.support.text.emoji.FontRequestEmojiCompatConfig;
+import android.support.v4.provider.FontRequest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initEmojiCompat();
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    private void initEmojiCompat() {
+        final EmojiCompat.Config config;
+            // Use a downloadable font for EmojiCompat
+            final FontRequest fontRequest = new FontRequest(
+                    "com.google.android.gms.fonts",
+                    "com.google.android.gms",
+                    "Noto Color Emoji Compat",
+                    R.array.com_google_android_gms_fonts_certs);
+            config = new FontRequestEmojiCompatConfig(getApplicationContext(), fontRequest);
+
+        config.setReplaceAll(true)
+                .registerInitCallback(new EmojiCompat.InitCallback() {
+                    @Override
+                    public void onInitialized() {
+                        Log.i(TAG, "EmojiCompat initialized");
+                    }
+
+                    @Override
+                    public void onFailed(@Nullable Throwable throwable) {
+                        Log.e(TAG, "EmojiCompat initialization failed", throwable);
+                    }
+                });
+        EmojiCompat.init(config);
+    }
+}
